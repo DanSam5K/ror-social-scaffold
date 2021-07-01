f9e130d0557a26b561d4e833bb76bb982e69e40d
Daniel Samuel <dansam5000@gmail.com>
2021-07-01 16:24:03 +0100

    Add status routes & Final touches

--- app/models/friendship.rb
+++ app/models/friendship.rb
@@ -1,6 +1,6 @@
 class Friendship < ApplicationRecord
-  scope :pending_requests, -> { where(status: false) }
-  scope :accepted_requests, -> { where(status: true) }
+  scope :pending_requests, -> { where(confirmed: false) }
+  scope :accepted_requests, -> { where(confirmed: true) }
 
   belongs_to :user
   belongs_to :friend, class_name: 'User'
