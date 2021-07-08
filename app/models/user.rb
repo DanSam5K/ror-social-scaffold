7580d96c2b81997f68f70baffe7a39dbde30dd52
Daniel Samuel <dansam5000@gmail.com>
2021-07-08 19:46:59 +0100

    Model user throug freinds with 2rolls for mutual friendship

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,6 +11,7 @@ class User < ApplicationRecord
   has_many :likes, dependent: :destroy
 
   has_many :friendships
+  has_many :friends, through: :friendships
   has_many :inverse_friendships, class_name: 'Friendship', foreign_key: 'friend_id'
 
   # Helper methods
