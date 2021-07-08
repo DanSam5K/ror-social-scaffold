e2bd7761f62beca252d1e5c5d97e321431b886b9
Daniel Samuel <dansam5000@gmail.com>
2021-07-08 23:36:15 +0100

    Create a reversed row when friendship is confirmed

--- app/models/friendship.rb
+++ app/models/friendship.rb
@@ -5,3 +5,12 @@ class Friendship < ApplicationRecord
   belongs_to :user
   belongs_to :friend, class_name: 'User'
 end
+
+def confirm_friend
+  self.update_attributes(confirmed: true)
+  Friendship.create!(
+      friend_id: self.user_id,
+      user_id: self.friend_id,
+      confirmed: true
+  )
+end
