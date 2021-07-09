153c95881e3789ea0d73bcec100bbca81bb882ef
Daniel Samuel <dansam5000@gmail.com>
2021-07-09 20:33:03 +0100

    Add negative test to Friendship model

--- spec/friendship_spec.rb
+++ spec/friendship_spec.rb
@@ -7,9 +7,19 @@ RSpec.describe User, type: :model do
       expect(friendship.macro).to eq(:belongs_to)
     end
 
+    it 'belongs to a user not has many' do
+      friendship = Friendship.reflect_on_association(:user)
+      expect(friendship.macro).not_to eq(:has_many)
+    end
+
     it 'belongs to a friend' do
       friendship = Friendship.reflect_on_association(:friend)
       expect(friendship.macro).to eq(:belongs_to)
+    end
+
+    it 'belongs to a friend not has many' do
+      friendship = Friendship.reflect_on_association(:friend)
+      expect(friendship.macro).not_to eq(:has_many)
     end
   end
 end
